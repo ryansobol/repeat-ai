9183cd837f2e78dbab8d30f674d245d08035d905
Ryan Sobol <hello@ryansobol.com>
2024-06-23 16:49:35 -0700

    initial

---
+++ src/lib/slices/RichText/index.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import type { Content } from '@prismicio/client';
+	import { PrismicRichText } from '@prismicio/svelte';
+
+	import Bounded from '$lib/components/Bounded.svelte';
+
+	export let slice: Content.RichTextSlice;
+</script>
+
+<Bounded>
+	<div class="prose prose-lg prose-slate prose-invert w-full">
+		<PrismicRichText field={slice.primary.content} />
+	</div>
+</Bounded>
